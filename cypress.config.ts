5304301e23bd92a82a62fa28c03660578c16c47c
adebola-xyz <adebolaakomolafe@gmail.com>
2022-11-29 11:42:07 +0100

    tests: header

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-   e2e: {
-      baseUrl: "http://localhost:3000",
-      setupNodeEvents(on, config) {
-         // implement node event listeners here
-      },
-   },
-   viewportHeight: 1117,
-   viewportWidth: 1728,
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  viewportHeight: 1117,
+  viewportWidth: 1728,
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
+    },
+  },
 });
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,9 +1,28 @@
 describe("The Home Page", () => {
-   it("successfully loads", () => {
+   before(() => {
       cy.visit("/");
    });
 
+   it("shows header", () => {
+      const header = cy.get("header");
+      header.should("be.visible");
+      header.get("nav > a").should("have.length", 4);
+      header.should("have.text", "Home");
+   });
+
+   it("shows header on mobile", () => {
+      cy.viewport("iphone-x");
+      const header = cy.get("header");
+      header.should("be.visible");
+   });
+
+   it("navigates to different collection from header", () => {
+      cy.get("header").get("nav").contains("New").click();
+      expect(cy.url()).to.be("/collections/new");
+      cy.contains("New Items").should("be.visible");
+   });
+
    it("shows collections", () => {
-      expect(cy.get(".collection")).to.be.visible();
+      cy.get(".collection").should("be.visible");
    });
 });
