563985a7ccb8de0e7a77f2574901e2f033f4bc24
adebola-xyz <adebolaakomolafe@gmail.com>
2022-12-29 14:50:28 +0100

    add: product page tests

--- cypress/e2e/item.cy.ts
+++ cypress/e2e/item.cy.ts
@@ -1,3 +1,48 @@
 describe("Test Product Page", () => {
-   it("navigates to product page", () => {});
+   it("navigates to product page and changes title", () => {
+      cy.visit("/");
+      const product = cy.get(".product-item").first();
+      product.then(($product) => {
+         // Prevent new tab opening.
+         $product.removeAttr("target");
+      });
+      cy.get(".product-item-heading")
+         .first()
+         .invoke("text")
+         .then(($text) => {
+            product.click();
+            cy.title().should("eq", `${$text} - Spry Clothing Store`);
+         });
+   });
+
+   describe("Test inside Product Page", () => {
+      beforeEach(() => {
+         cy.visit("/");
+         cy.get(".product-item")
+            .first()
+            .then(($product) => {
+               $product.removeAttr("target");
+            })
+            .click();
+      });
+
+      it("shows product image", () => {
+         cy.get("img#product-image").wait(3000).should("be.visible");
+      });
+
+      it("shows product name and price", () => {
+         cy.get("#product-name").should("be.viible");
+         cy.get("#product-price").should("be.visible");
+      });
+
+      it("shows tags", () => {
+         cy.get("#tags").scrollIntoView().should("be.visible");
+      });
+
+      it("shows related items", () => {
+         cy.get("#related-items").within(() => {
+            cy.get(".product-item").its("length").should("be.greaterThan", 1);
+         });
+      });
+   });
 });
