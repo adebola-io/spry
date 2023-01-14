426769e7d0dbd92b0da372be249c1100df58d4ff
adebola-xyz <adebolaakomolafe@gmail.com>
2023-01-14 13:17:15 +0100

    move: testing items to JSON files

--- data/testing/items/index.ts
+++ data/testing/items/index.ts
@@ -1,4 +1,7 @@
-import mens_suits from "./mens_suits";
-import womens_jackets from "./womens_jackets";
+import mens_suits from "./mens_suits.json";
+import womens_jackets from "./womens_jackets.json";
 
-export const items = [...mens_suits, ...womens_jackets];
+export const items = [
+   ...mens_suits,
+   ...womens_jackets,
+] as unknown as Item.Unit[];
